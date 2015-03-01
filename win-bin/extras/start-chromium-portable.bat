9bbd9f6f6a6908eff3b0134c20d1c62c25a207ca
pwlin <pwlin05@gmail.com>
2015-03-01 18:46:01 +0100

    Added extra chromium switches in example start-chromium-portable.bat

--- win-bin/extras/start-chromium-portable.bat
+++ win-bin/extras/start-chromium-portable.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET CRFOLDER=Z:\Path\To\Chromium\MainFolder
+SET CRFOLDER=%~dp0
 IF "%1" == "" (
     SET INDEX=%CRFOLDER%\index-portable.html
 ) ELSE (
@@ -8,5 +8,11 @@ IF "%1" == "" (
 )
 
 START %CRFOLDER%\App\Chrome-bin\chrome.exe  --user-data-dir="%CRFOLDER%\Data\profile" "%INDEX%"
-:: You can also add 
+:: Extra switches bases on http://peter.sh/experiments/chromium-command-line-switches/
 :: --disk-cache-dir="%TEMP%\ChromiumNightlyPortable" 
+:: --host-rules="MAP * baz, EXCLUDE www.google.com"
+:: --ignore-certificate-errors
+:: --ignore-gpu-blacklist
+:: --incognito
+:: --kiosk
+:: --disable-backing-store-limit  --disable-async-dns --disable-account-consistency --disable-affiliation-based-matching --disable-answers-in-suggest --disable-domain-reliability --disable-breakpad --disable-preconnect --disable-suggestions-service --disable-cloud-import --disable-logging --disable-default-apps --disable-component-cloud-policy --disable-sync --disable-translate --no-default-browser-check --ignore-autocomplete-off-autofill --disable-client-side-phishing-detection --safebrowsing-disable-auto-update
