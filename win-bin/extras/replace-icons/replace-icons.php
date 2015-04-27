56cca21f58cc3e24059397039554fc0403b2aed2
pwlin <pwlin05@gmail.com>
2015-04-27 19:11:07 +0200

    Fixed replace icons

--- win-bin/extras/replace-icons/replace-icons.php
+++ win-bin/extras/replace-icons/replace-icons.php
@@ -14,8 +14,8 @@ $srcDir = __DIR__ . DIRECTORY_SEPARATOR;
 
 $destPak = $prefs['unpack_dir'] . DIRECTORY_SEPARATOR . 'Chrome-bin' . DIRECTORY_SEPARATOR . $chromeExeVer . DIRECTORY_SEPARATOR . 'chrome_100_percent.pak';
 
-system('"' . $winBinDir . DIRECTORY_SEPARATOR . 'node.exe" "' . $srcDir .  'main.js" replace "' . $destPak . '" 7081 "' . $srcDir . 'icons' . DIRECTORY_SEPARATOR . 'folder-blue1.png"');
-system('"' . $winBinDir . DIRECTORY_SEPARATOR . 'node.exe" "' . $srcDir .  'main.js" replace "' . $destPak . '" 5714 "' . $srcDir . 'icons' . DIRECTORY_SEPARATOR . 'default_icon.png"');
+system('"' . $winBinDir . DIRECTORY_SEPARATOR . 'node.exe" "' . $srcDir .  'main.js" replace "' . $destPak . '" 7082 "' . $srcDir . 'icons' . DIRECTORY_SEPARATOR . 'folder-blue1.png"');
+system('"' . $winBinDir . DIRECTORY_SEPARATOR . 'node.exe" "' . $srcDir .  'main.js" replace "' . $destPak . '" 5715 "' . $srcDir . 'icons' . DIRECTORY_SEPARATOR . 'default_icon.png"');
 
 echo(PHP_EOL . 'Done.' . PHP_EOL . PHP_EOL);
 
